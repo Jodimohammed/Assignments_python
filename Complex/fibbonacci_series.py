c5788cf6442854eda33d883bf5979d064aa92893
jodi mohammed <jodimohammed82@gmail.com>
2024-01-03 22:14:47 -0500

    message

--- Complex/fibbonacci_series.py
+++ Complex/fibbonacci_series.py
@@ -1,7 +1,7 @@
 # python file for Fibonacci series:
 # the sum of two elements defines the next
 a, b = 0, 1
-while a < 100:
+while a < 1000:
     print(a)
 
     a, b = b, a+b
--- no_of_ass_done.py
+++ no_of_ass_done.py
@@ -1,6 +1,6 @@
 #file of how many i have done with in the assignments
 
 
-no = 28
+no = 27
 
 total =  100
