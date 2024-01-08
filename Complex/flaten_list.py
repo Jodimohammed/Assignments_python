5fbe6b6760e8554d0e7cd20ec619c0bdd4f4af0b
jodi mohammed <jodimohammed82@gmail.com>
2024-01-08 12:14:23 -0500

    flaten_list correction

--- Complex/flaten_list.py
+++ Complex/flaten_list.py
@@ -1,18 +1,18 @@
 #A python program to flaten a nested list. 
 
-def flaten_nested_list(nested_list):
-    flatened_list = []
+def flatten_nested_list(nested_list):
+    flattened_list = []
     for element in nested_list:
         if isinstance(element, list):
             for item in element:
-                    flatened_list.append(item)
+                    flattened_list.append(item)
 
         else:
-            flatened_list.append(element)
-    return flatened_list
+            flattened_list.append(element)
+    return flattened_list
 
 
 my_list = [1, 2, [3, 4],[5,6], 7, [8, 9, 10]]
 
-result = flaten_nested_list(my_list)
+result = flatten_nested_list(my_list)
 print( result)
