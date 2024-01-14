99d26b64387d17f0543b41a6785814575f4d09d8
jodi mohammed <jodimohammed82@gmail.com>
2024-01-14 00:30:08 -0500

    power file

---
+++ Complex/power_without**.py
@@ -0,0 +1,10 @@
+#python program for finding the power of a number without using ** operator
+
+import math
+
+num =int(input("please enter a number:"))
+power = int(input("please enter a power:"))
+
+product = math.pow(num,power)
+
+print(product)
